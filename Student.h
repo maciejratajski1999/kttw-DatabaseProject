b5ae97e54351dd112b14d19fb6741ff071245d4a
Maciej Ratajski <maciejratajski1999@gmail.com>
2022-05-02 21:32:49 +0200

    added Student header file

---
+++ Student.h
@@ -0,0 +1,16 @@
+#pragma once
+#include<string>
+class Student {
+	private:
+		int id;
+		std::string firstName;
+		std::string lastName;
+		int indexNumber;
+		std::string email;
+	public:
+		int getId;
+		std::string getFirstName;
+		std::string getLastName;
+		int getIndexNumber;
+		std::string getEmail;
+};
